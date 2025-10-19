c35cf3e8143d5b456dfaa4bb77e8e076369b9863
Rose Scott <snowm4808@gmail.com>
2025-10-20 00:15:37 +0100

    Changed it to add checks to see if it contains vidoe and audio and made it so it is still gapless when presenting the options to the user

--- Week 6 HELP MEEEEEEE ToT/Yt downlaoder/Yt down.py
+++ Week 6 HELP MEEEEEEE ToT/Yt downlaoder/Yt down.py
@@ -18,29 +18,33 @@ with YoutubeDL(ydl_opts) as ydl:
     info = ydl.extract_info(url, download=False)
 
 x = 0
-for x in range(len(info['formats'])): #Loop prints out all of the formats possible to use
-    format = info['formats'][x]
+valid_formats = []
+for format in info['formats']:
     if format['vcodec'] != 'none' and format['acodec'] != 'none':
-        print(f"the resolution is: {format['resolution']}")
-        print(f"The Format_id is: {format['format_id']}")
-        print(f"The Extension is: {format['ext']}")
-        print(f"This is option number {x}")
-        print("")
-        x = x + 1
+        valid_formats.append(format)
+
+for x in range(len(valid_formats)): #Loop prints out all of the formats possible to use
+    format = valid_formats[x]
+    print(f"the resolution is: {format['resolution']}")
+    print(f"The Format_id is: {format['format_id']}")
+    print(f"The Extension is: {format['ext']}")
+    print(f"This is option number {x}")
+    print("")
+    x = x + 1
 
 while True:
     try:
         User_choice = int(input("Enter the number of the option you wnat to download: "))
-        if 0 <= User_choice < len(info['formats']):
+        if 0 <= User_choice < len(valid_formats):
             break
         else:
-            print(f"Invalid choice. Please enter a number between 0 and {len(info['formats']) -1}.")
+            print(f"Invalid choice. Please enter a number between 0 and {len(valid_formats) -1}.")
     except ValueError:
         print("Invalid input. Please enter a number.")
 
 print("Valid choice selected!")
 
-selected_format = info['formats'][User_choice]
+selected_format = valid_formats[User_choice]
 format_id = selected_format['format_id']
 
 downlaod_opts = {'format': format_id}
