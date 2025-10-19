7dc5c8ae29c77ea8985f754f35e180b5ac3477a4
Rose Scott <snowm4808@gmail.com>
2025-10-19 22:13:42 +0100

    Am working on it slowley But it currently errors

--- Week 6 HELP MEEEEEEE ToT/Yt downlaoder/Yt down.py
+++ Week 6 HELP MEEEEEEE ToT/Yt downlaoder/Yt down.py
@@ -3,6 +3,8 @@ import yt_dlp
 import urllib3
 from yt_dlp import YoutubeDL
 
+#Testing URL = https://youtu.be/qQzdAsjWGPg?si=IgVwm9oxfSl3Qwml
+
 url = input("Enter URL of Video you want to Downlaod: ")
 
 ydl_opts = {}
@@ -15,4 +17,6 @@ print(len(info['format']))
 
 format = info['format'][0]
 
-print(format.keys())
+temp = format.keys()
+
+print(temp)
