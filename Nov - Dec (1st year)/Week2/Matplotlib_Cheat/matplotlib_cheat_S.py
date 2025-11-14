c1d81154b44f435a3ee7f08c87ab64dfa8931ab3
Rose <snowm4808@gmail.com>
2025-11-14 11:39:20 +0000

    Re added cheat sheet

---
+++ Nov - Dec (1st year)/Week2/Matplotlib_Cheat/matplotlib_cheat_S.py
@@ -0,0 +1,14 @@
+# Matplotlib cheat sheet
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3]
+y = [3, 2, 1]
+
+# Draws a grid on the background to make it easier to read
+plt.grid()
+
+# Shows the graph (ALWAYS THE LAST ONE TO BE POSTED)
+plt.show()
+
+# Plots a line on a line chart
+plt.plot(x, y)
