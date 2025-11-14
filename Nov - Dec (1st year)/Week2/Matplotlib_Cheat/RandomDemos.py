990638880e9d38329cb992758449180b7a5806e5
Rose <snowm4808@gmail.com>
2025-11-14 12:03:56 +0000

    broken

--- Nov - Dec (1st year)/Week2/Matplotlib_Cheat/RandomDemos.py
+++ Nov - Dec (1st year)/Week2/Matplotlib_Cheat/RandomDemos.py
@@ -1,9 +1,9 @@
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
 
 df = pd.read_csv("theme_park_rides.csv")
 
-#print(df.to_string(index = False))
+# print(df.to_string(index = False))
 
 averageWaitTimes = round(df.groupby("ride_name")["wait_time_minutes"].mean(), 2)
 
@@ -19,4 +19,30 @@ plt.grid()
 plt.show()
 
 
+df = pd.read_csv("theme_park_times.csv")
 
+print(df.to_String(index=False))
+# Converts data passed into function to datetime
+df["visit_timestamp"] = pd.to_datetime(df["visit_timestamp"])
+
+# creatrs two new collumns, called date and time, and stores them date and time data seperatly
+df["date"] = df["visit_timestamp"].dt.date
+df["time"] = df["visit_timestamp"].dt.time
+
+# built-in function to order values sp the graph dosnet plot incorrectly
+df2 = df.sort_values("visit_timestamp")
+
+areas = df["areas"].unique()
+
+for area in areas:
+    line_to_plot = df[df["area"] == area]
+    times_to_plot = []
+    for time in line_to_plot["time"]:
+        times_to_plot.append(str(time))
+    plt.plot(
+        line_to_plot["visit_timestamp"],
+        line_to_plot["number_of_people_visited"],
+        lable=area,
+    )
+    plt.xticks(rotation=90)
+    plt.show()
