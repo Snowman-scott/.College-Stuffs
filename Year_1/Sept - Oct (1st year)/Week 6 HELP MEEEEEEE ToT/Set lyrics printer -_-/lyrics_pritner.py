a302575486524782efa532fe3bc32818bda1307b
Epicwoman212 <snowm4808@proton.me>
2026-09-14 00:22:25 +0100

    UPD: STUFF

--- Year_1/Sept - Oct (1st year)/Week 6 HELP MEEEEEEE ToT/Set lyrics printer -_-/lyrics_pritner.py
+++ Year_1/Sept - Oct (1st year)/Week 6 HELP MEEEEEEE ToT/Set lyrics printer -_-/lyrics_pritner.py
@@ -95,32 +95,6 @@ def parse_lrc(synclyr):
     return results
 
 
-def tidal_search(lyrdata):
-    song_name = lyrdata["trackName"]
-    artist_name = lyrdata["artistName"]
-    album_name = lyrdata["albumName"]
-    track_length = lyrdata["duration"]
-    best = None
-    best_diff = 999
-    track_info = requests.get(
-        f"https://hund.qqdl.site/search/?s={song_name}&a={artist_name}&al={album_name}&limit=5"
-    )
-
-    search_data = track_info.json()["data"]
-
-    for item in search_data["items"]:
-        diff = abs(item["duration"] - track_length)
-        if diff < best_diff:
-            best_diff = diff
-            best = item
-
-    if best_diff <= 5:
-        song_id = best["id"]
-    else:
-        song_id = None
-    return song_id
-
-
 def youtube_search(lyrdata):
     song_name = lyrdata["trackName"]
     artist_name = lyrdata["artistName"]
@@ -145,29 +119,6 @@ def youtube_search(lyrdata):
         return best["url"]
     else:
         return None
-
-
-def get_song_url(lyrdata):
-    try:
-        song_id = tidal_search(lyrdata)
-        url = get_song(song_id)
-        if url:
-            return url
-    except Exception:
-        pass
-
-    return youtube_search(lyrdata)
-
-
-def get_song(song_id):
-    if song_id is not None:
-        song_request = requests.get(
-            f"https://hund.qqdl.site/trackManifests/?id={song_id}"
-        )
-        song_request_data = song_request.json()["data"]
-
-        return song_request_data["data"]["attributes"]["uri"]
-    return None
 
 
 def download_audio(url):
@@ -245,7 +196,7 @@ def main(lyrdata, url):
 lyrdata = select_track()
 lrclib_id(lyrdata)
 
-url = get_song_url(lyrdata)
+url = youtube_search(lyrdata)
 
 if url:
     main(lyrdata, url)
