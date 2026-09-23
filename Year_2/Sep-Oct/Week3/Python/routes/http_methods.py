80c2c742f5c7aaabe2c2b80fae413f59090e533d
Epicwoman212 <snowm4808@proton.me>
2026-09-23 14:26:46 +0100

    ADD: AAAHHHH 4

--- Year_2/Sep-Oct/Week3/Python/routes/http_methods.py
+++ Year_2/Sep-Oct/Week3/Python/routes/http_methods.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -16,10 +16,26 @@ def post():
 @app.route("/search")
 def search():
     if "item" in request.args.keys():
-        return(f"You searched for {request.args["item"]}")
+        return f"You searched for {request.args['item']}"
     else:
-        return("Please gib an item")
+        return "Please gib an item"
+
+
+@app.route("/register", methods=["POST"])
+def register():
+    try:
+        data = request.form
+
+
+        user = data["username"]
+        em = data["email"]
+        age = data["age"]
+        phone = data["phoneNumber"]
+    except KeyError as e:
+        return(f"You were missing a feild\nMake sure you have a username, email, age, and phone number \n\nError was: {e}")
+    else:
+        return(f"Hello {user} your email is {em} you are {age} old and your digits are {phone} :3")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
