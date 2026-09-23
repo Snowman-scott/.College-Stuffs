b631a0129acbd8ccd8045757f2ef455c83a60df5
Epicwoman212 <snowm4808@proton.me>
2026-09-23 12:19:56 +0100

    ADD: AAAHHHH 2

--- Year_2/Sep-Oct/Week3/Python/routes/http_methods.py
+++ Year_2/Sep-Oct/Week3/Python/routes/http_methods.py
@@ -2,9 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route("/", methods=["POST"])
 def home():
     return "Hello world"
+
+
+@app.route("/post", methods=["POST"])
+def post():
+    return "This is a post route"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
